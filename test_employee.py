93841f13e097cd75eb88faf0c11f95617cbcf7d2
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-04-02 14:30:01 +0200

    train references to objects
    nameclass.obj.property

--- test_employee.py
+++ test_employee.py
@@ -4,31 +4,31 @@ from employee import Employee
 
 class EmployeeTest(unittest.TestCase):
 
+    emp1 = Employee('Jan', 'Kowalski', 5000)
+    emp2 = Employee('Kuba', 'Nowak', 4000)
     def test_email(self):
-        emp1 = Employee('Jan', 'Kowalski', 5000)
-        emp2 = Employee('Kuba', 'Nowak', 4000)
 
-        self.assertEqual(emp1.email, 'Jan.Kowalski@email.com')
-        self.assertEqual(emp2.email, 'Kuba.Nowak@email.com')
 
-        emp1.first = 'Janek'
-        emp2.last = "Nowakowski"
+        self.assertEqual(EmployeeTest.emp1.email, 'Jan.Kowalski@email.com')
+        self.assertEqual(EmployeeTest.emp2.email, 'Kuba.Nowak@email.com')
 
-        self.assertEqual(emp1.email, 'Janek.Kowalski@email.com')
-        self.assertEqual(emp2.email, 'Kuba.Nowakowski@email.com')
+        EmployeeTest.emp1.first = 'Janek'
+        EmployeeTest.emp2.last = "Nowakowski"
+
+        self.assertEqual(EmployeeTest.emp1.email, 'Janek.Kowalski@email.com')
+        self.assertEqual(EmployeeTest.emp2.email, 'Kuba.Nowakowski@email.com')
 
     def test_apply_raise(self):
-        emp1 = Employee('Jan', 'Kowalski', 5000)
-        emp2 = Employee('Kuba', 'Nowak', 4000)
+        
 
-        self.assertEqual(emp1.pay, 5000)
-        self.assertEqual(emp2.pay, 4000)
+        self.assertEqual(EmployeeTest.emp1.pay, 5000)
+        self.assertEqual(EmployeeTest.emp2.pay, 4000)
 
-        emp1.apply_raise()
-        emp2.apply_raise()
+        EmployeeTest.emp1.apply_raise()
+        EmployeeTest.emp2.apply_raise()
 
-        self.assertEqual(emp1.pay, 1.05*5000)
-        self.assertEqual(emp2.pay, 1.05*4000)
+        self.assertEqual(EmployeeTest.emp1.pay, 1.05*5000)
+        self.assertEqual(EmployeeTest.emp2.pay, 1.05*4000)
 
 
 if __name__ == '__main__':
